543951b54cfe8900a78536eb7503273526512235
jordanoboyle <jmcmcode@gmail.com>
2024-06-04 12:56:16 -0600

    HorsesIndex rendering hardcode test

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,15 @@
+import { HorsesIndex } from "./HorsesIndex"
+
 export function Content() {
+
+  const horses = [
+    {size: "big", color: "black", price: 8000}
+  ]
+
   return (
     <main>
       <h1>Welcome to React!</h1>
+      <HorsesIndex horses={horses}/>
     </main>
   )
 }
---
+++ src/HorsesIndex.jsx
@@ -0,0 +1,12 @@
+
+
+export function HorsesIndex(props) {
+  return(
+    <di>
+      Testing some horse text here
+      <p>Size: {props.horses[0].size}</p>
+      <p>Color: {props.horses[0].color}</p>
+      <p>price: {props.horses[0].price}</p>
+    </di>
+  )
+}
